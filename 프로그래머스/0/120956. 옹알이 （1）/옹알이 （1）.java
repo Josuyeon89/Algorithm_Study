2bb143a62e750af0d7e64f9daf952c4ed50ed779
Josuyeon89 <138758347+Josuyeon89@users.noreply.github.com>
2024-09-10 14:33:17 +1000

    [level 0] Title: 옹알이 (1), Time: 0.84 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int count=0;
+        String[] speak = {"aya", "ye", "woo", "ma"};
+        for(int i=0 ; i<babbling.length ; i++){            
+            for(int j=0; j<speak.length;j++){
+                if(babbling[i].contains(speak[j]))
+                    babbling[i]=babbling[i].replace(speak[j], " ");
+            }
+            if(babbling[i].isBlank()) //isBlank는 널값+공백, isEmpty는 널값만
+                count++;       
+        }
+              
+        return count;
+    }
+}
