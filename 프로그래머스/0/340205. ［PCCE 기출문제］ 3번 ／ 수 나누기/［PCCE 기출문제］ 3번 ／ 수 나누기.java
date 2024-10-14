7b4b8dead885cf134198edcf466c9842ca8254cd
Josuyeon89 <138758347+Josuyeon89@users.noreply.github.com>
2024-10-14 15:53:30 +1100

    [ PCCE 기출문제］ 3번 ／ 수 나누기.java

--- 프로그래머스/0/340205. ［PCCE 기출문제］ 3번 ／ 수 나누기/［PCCE 기출문제］ 3번 ／ 수 나누기.java
+++ 프로그래머스/0/340205. ［PCCE 기출문제］ 3번 ／ 수 나누기/［PCCE 기출문제］ 3번 ／ 수 나누기.java
@@ -6,7 +6,7 @@ public class Solution {
         int number = sc.nextInt();
         int answer = 0;
         
-        while(number>0){
+        while(number>0){    //Math함수를 쓰면 홀수자리일때 불가능, num이 0일때까지 반복하면 자리수에 관계없이 두 자리씩 나눌 수 있음 
             answer += number % 100;
             number /= 100;
         }
